2c33f93f417cf567c1dac62828778aa0df51a870
AlainAdel <53158091+AlainAdel@users.noreply.github.com>
2022-03-10 21:43:11 -0500

    Add files via upload

---
+++ Week5/Program6.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author Alain Fathalla
+ *
+ */
+public class Program6 {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println("The sum is " + sum);
+		
+		
+		// Write a for loop to calculate the sum of all the numbers between 20 and 150
+		
+		
+		int tot = 0;
+		for (int i = 20; i <= 150; i++) {
+			tot += i;
+		}
+		System.out.println("The sum is " + tot);
+		
+	}
+
+}
---
+++ Week5/Review.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+public class Review {
+	public static void main (String[]args) {
+		Random r = new Random();
+		int a,b,c;
+		a = 7;
+		b = 8;
+		c = 10;
+		
+		if (a > b) {
+			System.out.println("A is greater");
+		} else if (b < c) {
+			System.out.println("C is greater than b");
+		} else {
+			System.out.println("Ok A is not greater than b and b is not less than c");
+		}
+		int the_number = r.nextInt(5) + 1;
+	
+		switch(the_number) {
+	
+		case 1:{
+			System.out.println("The value is 1");
+			break;
+		}
+		case 2:{
+			System.out.println("The value is 2");
+			break;
+		}
+		case 3:{
+			System.out.println("The value is 3");
+			break;
+		}
+		default:
+			System.out.println("default: The value is " + the_number);
+		}
+		
+	}
+	
+}
